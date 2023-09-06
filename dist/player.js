3c265356182fa162f40948827b8eb5af56801413
Max Spiridonov <vorgoronn@optic.xyz>
2023-09-06 20:42:20 +0400

    Implement youtube api

--- dist/player.js
+++ dist/player.js
@@ -683,73 +683,182 @@
   };
 
   // src/audio/youtube.ts
-  var YouTubeAPI = class {
-    linkIsReady;
-    player;
-    constructor() {
-      this.linkIsReady = false;
-      this.player = null;
-      this.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady.bind(this);
-    }
-    onYouTubeIframeAPIReady() {
-      this.linkIsReady = true;
-    }
-    static getYoutubeVideoID(url) {
-      try {
-        let urlObject = new URL(url);
-        let params = new URLSearchParams(urlObject.search);
-        return params.get("v");
-      } catch (e) {
-        console.error("\u041D\u0435\u0432\u0435\u0440\u043D\u044B\u0439 URL", e);
-        return null;
-      }
-    }
-  };
   var YouTubePlayer = class {
-    elementId;
     player;
-    constructor(elementId) {
-      this.elementId = elementId;
-      this.player = null;
+    playPauseBtn;
+    progressSlider;
+    track;
+    dragging;
+    progressInterval = 0;
+    constructor(playPauseBtn, progressSlider, track) {
+      this.playPauseBtn = playPauseBtn;
+      this.progressSlider = progressSlider;
+      this.track = track;
+      this.dragging = false;
+      this.progressInterval = 0;
+      this.initListeners();
     }
-    init(videoId) {
-      this.player = new YT.Player(this.elementId, {
-        height: "480",
-        width: "480",
-        videoId,
-        playerVars: {
-          controls: 0,
-          disablekb: 1,
-          modestbranding: 1,
-          rel: 0,
-          showinfo: 0,
-          autoplay: 0,
-          fs: 0
-        },
+    initListeners() {
+      this.playPauseBtn.addEventListener("click", () => this.playPauseVideo());
+      this.progressSlider.addEventListener("mousedown", (e) => this.mouseDown(e));
+      document.addEventListener("mousemove", (e) => this.mouseMove(e));
+      document.addEventListener("mouseup", () => this.mouseUp());
+    }
+    onYouTubeIframeAPIReady(videoID) {
+      this.player = new YT.Player("youtube-player", {
+        height: "48",
+        width: "48",
+        videoId: videoID,
         events: {
-          onReady: (event) => {
-            event.target.playVideo();
-          },
-          onStateChange: (event) => {
-            console.log("\u0421\u043E\u0441\u0442\u043E\u044F\u043D\u0438\u0435 \u043F\u043B\u0435\u0435\u0440\u0430: " + event.data);
-            if (event.data == YT.PlayerState.ENDED) {
-              alert("\u0412\u0438\u0434\u0435\u043E \u0437\u0430\u043A\u043E\u043D\u0447\u0438\u043B\u043E\u0441\u044C");
-            }
-          }
+          onReady: () => this.onPlayerReady(),
+          onStateChange: (event) => this.onPlayerStateChange(event)
         }
       });
     }
+    onPlayerReady() {
+      this.player.setVolume(30);
+    }
+    playPauseVideo() {
+      const state = this.player.getPlayerState();
+      if (state === YT.PlayerState.ENDED) {
+        this.player.seekTo(0);
+        this.player.playVideo();
+      }
+      if (state === YT.PlayerState.PAUSED || state === YT.PlayerState.CUED) {
+        this.player.playVideo();
+        this.playPauseBtn.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                <rect x="2.5" y="1.5" width="4" height="13" rx="1.5" stroke="white"/>
+                <rect x="9.5" y="1.5" width="4" height="13" rx="1.5" stroke="white"/>
+            </svg>`;
+        this.progressInterval = setInterval(() => {
+          if (!this.dragging) {
+            const progress = this.player.getCurrentTime() / this.player.getDuration() * 100;
+            this.track.style.width = progress + "%";
+            if (progress >= 100) {
+              this.finishVideo();
+            }
+          }
+        }, 1e3);
+      } else {
+        this.player.pauseVideo();
+        this.playPauseBtn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                <path d="M13.3375 10.8944L21.7598 15.1056C22.4968 15.4741 22.4968 16.5259 21.7598 16.8944L13.3375 21.1056C12.3402 21.6042 11.1667 20.879 11.1667 19.7639V12.2361C11.1667 11.121 12.3402 10.3958 13.3375 10.8944Z" stroke="white"/>
+            </svg>`;
+        clearInterval(this.progressInterval);
+      }
+    }
+    onPlayerStateChange(event) {
+      if (event.data === YT.PlayerState.ENDED) {
+        this.finishVideo();
+      }
+    }
+    mouseDown(e) {
+      this.dragging = true;
+      clearInterval(this.progressInterval);
+      this.updateProgress(e);
+    }
+    mouseMove(e) {
+      if (this.dragging) {
+        this.updateProgress(e);
+      }
+    }
+    mouseUp() {
+      this.dragging = false;
+      this.progressInterval = setInterval(() => {
+        if (!this.dragging) {
+          const progress = this.player.getCurrentTime() / this.player.getDuration() * 100;
+          this.track.style.width = progress + "%";
+          if (progress >= 100) {
+            this.finishVideo();
+          }
+        }
+      }, 1e3);
+    }
+    updateProgress(e) {
+      const rect = this.progressSlider.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const width = rect.right - rect.left;
+      const progress = Math.min(Math.max(x / width, 0), 1) * 100;
+      this.track.style.width = progress + "%";
+      this.player.seekTo(this.player.getDuration() * (progress / 100));
+      if (progress >= 100) {
+        this.finishVideo();
+      }
+    }
+    finishVideo() {
+      this.playPauseBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+            <path d="M13.3375 10.8944L21.7598 15.1056C22.4968 15.4741 22.4968 16.5259 21.7598 16.8944L13.3375 21.1056C12.3402 21.6042 11.1667 20.879 11.1667 19.7639V12.2361C11.1667 11.121 12.3402 10.3958 13.3375 10.8944Z" stroke="white"/>
+        </svg>`;
+      clearInterval(this.progressInterval);
+    }
+  };
+  var YoutubePlayerContainer = class {
+    containerId = "";
+    container;
+    videoId = "";
+    youtubePlayer = null;
+    name = "";
+    constructor(containerId, videoId, name) {
+      this.containerId = containerId;
+      this.container = document.getElementById(containerId);
+      this.videoId = videoId;
+      this.name = name;
+      this.initializePlayer();
+    }
+    async loadYouTubeIframeAPI() {
+      return new Promise((resolve, reject) => {
+        window.onYouTubeIframeAPIReady = () => {
+          resolve();
+        };
+        const tag = document.createElement("script");
+        tag.src = "https://www.youtube.com/iframe_api";
+        tag.onerror = reject;
+        const firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
+      });
+    }
+    async initializePlayer() {
+      if (!this.container)
+        return;
+      this.container.classList.add("aiornot-player-square");
+      this.container.innerHTML = `
+            <div id="youtube-player"></div>
+            <div class="aiornot-player-controls-square">
+                <div class="aiornot-player-button-sqaure" id="${this.container.id}-playPauseBtn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                        <path d="M13.3375 10.8944L21.7598 15.1056C22.4968 15.4741 22.4968 16.5259 21.7598 16.8944L13.3375 21.1056C12.3402 21.6042 11.1667 20.879 11.1667 19.7639V12.2361C11.1667 11.121 12.3402 10.3958 13.3375 10.8944Z" stroke="white"/>
+                    </svg>
+                </div>
+                <div class="aiornot-player-hover-bg"></div>
+                <div class="aiornot-player-title-sqaure">${this.name}</div>
+            </div>
+            <div id="${this.container.id}-slider" class="aiornot-player-slider">
+                <div id="${this.container.id}-progress" class="aiornot-player-progress"></div>
+            </div>
+        `;
+      const playPauseBtn = document.getElementById(`${this.container.id}-playPauseBtn`);
+      const progressSlider = document.getElementById(`${this.container.id}-slider`);
+      const track = document.getElementById(`${this.container.id}-progress`);
+      await this.loadYouTubeIframeAPI();
+      this.youtubePlayer = new YouTubePlayer(playPauseBtn, progressSlider, track);
+      this.youtubePlayer.onYouTubeIframeAPIReady(this.videoId);
+    }
   };
   var createYoutubePlayer = (elementId, url) => {
-    const youTubeAPI = new YouTubeAPI();
-    const youTubePlayer = new YouTubePlayer(elementId);
-    let videoID = YouTubeAPI.getYoutubeVideoID(url);
-    if (videoID) {
-      console.log("ID \u0432\u0438\u0434\u0435\u043E: " + videoID);
-      youTubePlayer.init(videoID);
-    } else {
-      console.log("\u041D\u0435 \u0443\u0434\u0430\u043B\u043E\u0441\u044C \u043F\u043E\u043B\u0443\u0447\u0438\u0442\u044C ID \u0432\u0438\u0434\u0435\u043E");
-    }
+    const getYoutubeVideoID = (url2) => {
+      try {
+        let urlObject = new URL(url2);
+        let params = new URLSearchParams(urlObject.search);
+        return params.get("v") ?? "";
+      } catch (e) {
+        console.error("\u041D\u0435\u0432\u0435\u0440\u043D\u044B\u0439 URL", e);
+        return "";
+      }
+    };
+    const videoID = getYoutubeVideoID(url);
+    new YoutubePlayerContainer(elementId, videoID, "");
   };
 
   // src/dashboard/AudioElements.ts
--- src/audio/youtube.ts
+++ src/audio/youtube.ts
@@ -1,78 +1,270 @@
-export class YouTubeAPI {
-    linkIsReady: boolean;
+export class YouTubePlayer {
     player: any;
+    playPauseBtn: HTMLElement;
+    progressSlider: HTMLElement;
+    track: HTMLElement;
+    dragging: boolean;
+    progressInterval: number = 0;
 
-    constructor() {
-        this.linkIsReady = false;
-        this.player = null;
-
-        this.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady.bind(this);
+    constructor(playPauseBtn: HTMLElement, progressSlider: HTMLElement, track: HTMLElement) {
+        this.playPauseBtn = playPauseBtn;
+        this.progressSlider = progressSlider;
+        this.track = track;
+        this.dragging = false;
+        this.progressInterval = 0;
+        this.initListeners();
     }
 
-    onYouTubeIframeAPIReady(): void {
-        this.linkIsReady = true;
+    initListeners(): void {
+        this.playPauseBtn.addEventListener('click', () => this.playPauseVideo());
+        this.progressSlider.addEventListener('mousedown', (e: MouseEvent) => this.mouseDown(e));
+        document.addEventListener('mousemove', (e: MouseEvent) => this.mouseMove(e));
+        document.addEventListener('mouseup', () => this.mouseUp());
     }
 
-    static getYoutubeVideoID(url: string): string | null {
-        try {
-            let urlObject = new URL(url);
-            let params = new URLSearchParams(urlObject.search);
-            return params.get('v');
-        } catch (e) {
-            console.error('Неверный URL', e);
-            return null;
+    onYouTubeIframeAPIReady(videoID: string): void {
+        // @ts-ignore
+        this.player = new YT.Player('youtube-player', {
+            height: '48',
+            width: '48',
+            videoId: videoID,
+            events: {
+                onReady: () => this.onPlayerReady(),
+                onStateChange: (event: any) => this.onPlayerStateChange(event),
+            },
+        });
+    }
+
+    onPlayerReady(): void {
+        this.player.setVolume(30);
+    }
+
+    playPauseVideo(): void {
+        const state = this.player.getPlayerState();
+
+        // @ts-ignore
+        if (state === YT.PlayerState.ENDED) {
+            this.player.seekTo(0);
+            this.player.playVideo();
         }
+
+        // @ts-ignore
+        if (state === YT.PlayerState.PAUSED || state === YT.PlayerState.CUED) {
+            this.player.playVideo();
+            this.playPauseBtn.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                <rect x="2.5" y="1.5" width="4" height="13" rx="1.5" stroke="white"/>
+                <rect x="9.5" y="1.5" width="4" height="13" rx="1.5" stroke="white"/>
+            </svg>`;
+            this.progressInterval = setInterval(() => {
+                if (!this.dragging) {
+                    const progress = (this.player.getCurrentTime() / this.player.getDuration()) * 100;
+                    this.track.style.width = progress + '%';
+                    if (progress >= 100) {
+                        this.finishVideo();
+                    }
+                }
+            }, 1000);
+        } else {
+            this.player.pauseVideo();
+            this.playPauseBtn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                <path d="M13.3375 10.8944L21.7598 15.1056C22.4968 15.4741 22.4968 16.5259 21.7598 16.8944L13.3375 21.1056C12.3402 21.6042 11.1667 20.879 11.1667 19.7639V12.2361C11.1667 11.121 12.3402 10.3958 13.3375 10.8944Z" stroke="white"/>
+            </svg>`;
+            clearInterval(this.progressInterval);
+        }
+    }
+
+    onPlayerStateChange(event: any): void {
+        // @ts-ignore
+        if (event.data === YT.PlayerState.ENDED) {
+            this.finishVideo();
+        }
+    }
+
+    mouseDown(e: MouseEvent): void {
+        this.dragging = true;
+        clearInterval(this.progressInterval);
+        this.updateProgress(e);
+    }
+
+    mouseMove(e: MouseEvent): void {
+        if (this.dragging) {
+            this.updateProgress(e);
+        }
+    }
+
+    mouseUp(): void {
+        this.dragging = false;
+        this.progressInterval = setInterval(() => {
+            if (!this.dragging) {
+                const progress = (this.player.getCurrentTime() / this.player.getDuration()) * 100;
+                this.track.style.width = progress + '%';
+                if (progress >= 100) {
+                    this.finishVideo();
+                }
+            }
+        }, 1000);
+    }
+
+    updateProgress(e: MouseEvent): void {
+        const rect = this.progressSlider.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const width = rect.right - rect.left;
+        const progress = Math.min(Math.max(x / width, 0), 1) * 100;
+        this.track.style.width = progress + '%';
+        this.player.seekTo(this.player.getDuration() * (progress / 100));
+        if (progress >= 100) {
+            this.finishVideo();
+        }
+    }
+
+    finishVideo(): void {
+        this.playPauseBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+            <path d="M13.3375 10.8944L21.7598 15.1056C22.4968 15.4741 22.4968 16.5259 21.7598 16.8944L13.3375 21.1056C12.3402 21.6042 11.1667 20.879 11.1667 19.7639V12.2361C11.1667 11.121 12.3402 10.3958 13.3375 10.8944Z" stroke="white"/>
+        </svg>`;
+        clearInterval(this.progressInterval);
     }
 }
 
-export class YouTubePlayer {
-    elementId: string;
-    player: any;
+export class YoutubePlayerContainer {
+    containerId: string = '';
+    container: HTMLElement | null;
+    videoId: string = '';
+    youtubePlayer: YouTubePlayer | null = null;
+    name: string = '';
 
-    constructor(elementId: string) {
-        this.elementId = elementId;
-        this.player = null;
+    constructor(containerId: string, videoId: string, name: string) {
+        this.containerId = containerId;
+        this.container = document.getElementById(containerId);
+        this.videoId = videoId;
+        this.name = name;
+        this.initializePlayer();
     }
 
-    init(videoId: string): void {
-        // @ts-ignore
-        this.player = new YT.Player(this.elementId, {
-            height: '480',
-            width: '480',
-            videoId: videoId,
-            playerVars: {
-                controls: 0,
-                disablekb: 1,
-                modestbranding: 1,
-                rel: 0,
-                showinfo: 0,
-                autoplay: 0,
-                fs: 0,
-            },
-            events: {
-                onReady: (event: any) => {
-                    event.target.playVideo();
-                },
-                onStateChange: (event: any) => {
-                    console.log('Состояние плеера: ' + event.data);
-                    //@ts-ignore
-                    if (event.data == YT.PlayerState.ENDED) {
-                        alert('Видео закончилось');
-                    }
-                },
-            },
+    async loadYouTubeIframeAPI() {
+        return new Promise((resolve: any, reject) => {
+            // @ts-ignore
+            window.onYouTubeIframeAPIReady = () => {
+                resolve();
+            };
+            const tag = document.createElement('script') as HTMLScriptElement;
+            tag.src = 'https://www.youtube.com/iframe_api';
+            tag.onerror = reject;
+            const firstScriptTag = document.getElementsByTagName('script')[0] as HTMLScriptElement;
+            firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
         });
+    }
+
+    async initializePlayer(): Promise<void> {
+        if (!this.container) return;
+
+        this.container.classList.add('aiornot-player-square');
+        this.container.innerHTML = `
+            <div id="youtube-player"></div>
+            <div class="aiornot-player-controls-square">
+                <div class="aiornot-player-button-sqaure" id="${this.container.id}-playPauseBtn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                        <path d="M13.3375 10.8944L21.7598 15.1056C22.4968 15.4741 22.4968 16.5259 21.7598 16.8944L13.3375 21.1056C12.3402 21.6042 11.1667 20.879 11.1667 19.7639V12.2361C11.1667 11.121 12.3402 10.3958 13.3375 10.8944Z" stroke="white"/>
+                    </svg>
+                </div>
+                <div class="aiornot-player-hover-bg"></div>
+                <div class="aiornot-player-title-sqaure">${this.name}</div>
+            </div>
+            <div id="${this.container.id}-slider" class="aiornot-player-slider">
+                <div id="${this.container.id}-progress" class="aiornot-player-progress"></div>
+            </div>
+        `;
+
+        const playPauseBtn = document.getElementById(`${this.container.id}-playPauseBtn`) as HTMLElement;
+        const progressSlider = document.getElementById(`${this.container.id}-slider`) as HTMLElement;
+        const track = document.getElementById(`${this.container.id}-progress`) as HTMLElement;
+        await this.loadYouTubeIframeAPI();
+        this.youtubePlayer = new YouTubePlayer(playPauseBtn, progressSlider, track);
+        this.youtubePlayer.onYouTubeIframeAPIReady(this.videoId);
     }
 }
 
 export const createYoutubePlayer = (elementId: string, url: string): void => {
-    const youTubeAPI = new YouTubeAPI();
-    const youTubePlayer = new YouTubePlayer(elementId);
-    let videoID: string | null = YouTubeAPI.getYoutubeVideoID(url);
-    if (videoID) {
-        console.log('ID видео: ' + videoID);
-        youTubePlayer.init(videoID);
-    } else {
-        console.log('Не удалось получить ID видео');
-    }
+    const getYoutubeVideoID = (url: string): string => {
+        try {
+            let urlObject = new URL(url);
+            let params = new URLSearchParams(urlObject.search);
+            return params.get('v') ?? '';
+        } catch (e) {
+            console.error('Неверный URL', e);
+            return '';
+        }
+    };
+
+    const videoID: string = getYoutubeVideoID(url);
+
+    new YoutubePlayerContainer(elementId, videoID, '');
 };
+
+// export class YouTubeAPI {
+//     linkIsReady: boolean;
+//     player: any;
+
+//     constructor() {
+//         this.linkIsReady = false;
+//         this.player = null;
+
+//         this.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady.bind(this);
+//     }
+
+//     onYouTubeIframeAPIReady(): void {
+//         this.linkIsReady = true;
+//     }
+
+//     static getYoutubeVideoID(url: string): string | null {
+//         try {
+//             let urlObject = new URL(url);
+//             let params = new URLSearchParams(urlObject.search);
+//             return params.get('v');
+//         } catch (e) {
+//             console.error('Неверный URL', e);
+//             return null;
+//         }
+//     }
+// }
+
+// export class YouTubePlayer {
+//     elementId: string;
+//     player: any;
+
+//     constructor(elementId: string) {
+//         this.elementId = elementId;
+//         this.player = null;
+//     }
+
+//     init(videoId: string): void {
+//         // @ts-ignore
+//         this.player = new YT.Player(this.elementId, {
+//             height: '480',
+//             width: '480',
+//             videoId: videoId,
+//             playerVars: {
+//                 controls: 0,
+//                 disablekb: 1,
+//                 modestbranding: 1,
+//                 rel: 0,
+//                 showinfo: 0,
+//                 autoplay: 0,
+//                 fs: 0,
+//             },
+//             events: {
+//                 onReady: (event: any) => {
+//                     event.target.playVideo();
+//                 },
+//                 onStateChange: (event: any) => {
+//                     console.log('Состояние плеера: ' + event.data);
+//                     //@ts-ignore
+//                     if (event.data == YT.PlayerState.ENDED) {
+//                         alert('Видео закончилось');
+//                     }
+//                 },
+//             },
+//         });
+//     }
+// }
