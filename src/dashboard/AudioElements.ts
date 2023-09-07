bac41f95b31803240f6bae77c2fbafb45b1b8952
Max Spiridonov <vorgoronn@optic.xyz>
2023-09-08 00:15:06 +0400

    fixed report screen on the audio page

--- src/dashboard/AudioElements.ts
+++ src/dashboard/AudioElements.ts
@@ -1,30 +1,27 @@
-import { initFingerPrint, visitorId } from '$utils/fingerprint';
+import { initFingerPrint } from '$utils/fingerprint';
 import { AudioPlayerContainer, PlayerManager, createYoutubePlayer } from '@/audio';
-import { AuthService, BASE_URL_RESULTS, OpenAIGeneratedService, RequestCounter, WrapperAIGeneratedService } from '../api';
+import { AuthService, BASE_URL_RESULTS, RequestCounter, WrapperAIGeneratedService } from '../api';
 
 export const initAudio = () => {
     //elements
     const reportScreen = document.getElementById('report-screen') as any;
     const reportButton_submit = document.querySelector('#button-report-submit') as Element;
     const reportInput = document.querySelector('#input-report-comment') as any;
-    const reportButton_true = document.querySelector('#button-report_true') as Element;
-    const reportButton_false = document.querySelector('#button-report_false') as Element;
-    const reportButton_close = document.querySelector('#button-report_close') as any;
-    const uiEl_urlError = document.querySelector('#url-error-message') as Element;
-    const buttonEl_processClose = document.querySelector('#processing_cancel') as Element;
-    const inputEl_fileInput = document.querySelector('#file-input') as any;
-    const imageEl_currentImage = document.querySelector('#ai-or-not-current-image') as any;
-    const imageEl_currentImageEmpty = document.querySelector('#empty-preview-img') as Element;
-    const imageEl_nsfwImage = document.querySelector('#nsfw-preview-img') as Element;
-    const textEl_inputError = document.querySelector('#input-error-text') as Element;
+    const reportButtonLike = document.querySelector('#button-report_true') as Element;
+    const reportButtonDislike = document.querySelector('#button-report_false') as Element;
+    const reportScreenCloseButton = document.querySelector('#button-report_close') as any;
+    const errorMessage = document.querySelector('#url-error-message') as Element;
+    const cancelProcessingButton = document.querySelector('#processing_cancel') as Element;
+    const fileInput = document.querySelector('#file-input') as any;
+    const fileInputErrorMessage = document.querySelector('#input-error-text') as Element;
     const youtubeLinkInput = document.getElementById('ai-or-not_image-url') as any;
     const checkYoutubeLinkButton = document.getElementById('ai-or-not_submit') as Element;
-    const uiEl_dropZone = document.querySelector('#ai-or-not_dropzone') as Element;
-    const textEl_dropZoneError = document.querySelector('#ai-or-not_dropzone-text') as Element;
-    const uiEl_resultCol = document.querySelector('#result-screen_col') as Element;
-    const buttonEl_sharedButtons = document.querySelector('#share-items-hide') as Element;
-    const counterEl_requestCounterValue = document.querySelector('#ai-or-not-dropzone-counter') as Element;
-    const counterEl_requestCounterBlock = document.querySelector('#ai-or-not-dropzone-counter-w') as Element;
+    const dropZone = document.querySelector('#ai-or-not_dropzone') as Element;
+    const dropZoneErrorMessage = document.querySelector('#ai-or-not_dropzone-text') as Element;
+    const resultContainer = document.querySelector('#result-screen_col') as Element;
+    const shareButtonsContainer = document.querySelector('#share-items-hide') as Element;
+    const dropZoneRequestCounter = document.querySelector('#ai-or-not-dropzone-counter') as Element;
+    const dropZoneRequestCounterContainer = document.querySelector('#ai-or-not-dropzone-counter-w') as Element;
 
     let fileSizeAllow: any;
     let currentResultId: any;
@@ -32,34 +29,18 @@ export const initAudio = () => {
     const updateRequestCounter = () => {
         if (!AuthService.isExpiredToken()) {
             // Hide element
-            counterEl_requestCounterBlock?.classList.add('hide');
+            dropZoneRequestCounterContainer?.classList.add('hide');
         } else {
             // Increment the count and show element
             const value = localStorage.getItem('requestCount') || '0';
-            counterEl_requestCounterValue.textContent = Number(value) <= 5 ? value : '5';
-            counterEl_requestCounterBlock.classList.remove('hide');
+            dropZoneRequestCounter.textContent = Number(value) <= 5 ? value : '5';
+            dropZoneRequestCounterContainer.classList.remove('hide');
         }
     };
 
     updateRequestCounter();
 
     initFingerPrint();
-
-    const uiReported_false = () => {
-        let buttonText = document.querySelector('#button-report_false-text') as Element;
-        buttonText.classList.remove('hide');
-        buttonText.textContent = buttonText.getAttribute('report-button-text-default_reported');
-        reportButton_false.classList.add('is-reported');
-        reportButton_true.classList.add('hide');
-    };
-
-    const uiReported_true = () => {
-        let buttonText = document.querySelector('#button-report_true-text') as Element;
-        buttonText.classList.remove('hide');
-        buttonText.textContent = buttonText.getAttribute('report-button-text-default_reported');
-        reportButton_true.classList.add('is-reported');
-        reportButton_false.classList.add('hide');
-    };
 
     const uiReported_initialState = () => {
         reportInput.value = '';
@@ -72,10 +53,10 @@ export const initAudio = () => {
         buttonText_true.textContent = buttonText_true.getAttribute('report-button-text-default');
         buttonText_false.textContent = buttonText_false.getAttribute('report-button-text-default');
 
-        reportButton_true.classList.remove('is-reported');
-        reportButton_false.classList.remove('is-reported');
-        reportButton_true.classList.remove('hide');
-        reportButton_false.classList.remove('hide');
+        reportButtonLike.classList.remove('is-reported');
+        reportButtonDislike.classList.remove('is-reported');
+        reportButtonLike.classList.remove('hide');
+        reportButtonDislike.classList.remove('hide');
     };
 
     const changeShareUrl = (responseId: string) => {
@@ -95,27 +76,27 @@ export const initAudio = () => {
     };
 
     const fileSizeMessage_ok = () => {
-        textEl_dropZoneError.textContent = 'We support jpeg, png, webp, gif, tiff, bmp. 10 Mb of maximum size.';
-        textEl_dropZoneError.classList.remove('text-color-red');
-        textEl_inputError.textContent = 'Something went wrong. Try again.';
-        uiEl_urlError.classList.add('hide');
+        dropZoneErrorMessage.textContent = 'We support jpeg, png, webp, gif, tiff, bmp. 10 Mb of maximum size.';
+        dropZoneErrorMessage.classList.remove('text-color-red');
+        fileInputErrorMessage.textContent = 'Something went wrong. Try again.';
+        errorMessage.classList.add('hide');
     };
 
     const someThingWentWrong_error = () => {
-        uiEl_urlError.classList.remove('hide');
+        errorMessage.classList.remove('hide');
     };
 
     const someThingWentWrong_ok = () => {
-        uiEl_urlError.classList.add('hide');
+        errorMessage.classList.add('hide');
     };
 
     const fileSizeMessage_error = () => {
-        if (uiEl_resultCol.classList.contains('hide')) {
-            textEl_dropZoneError.textContent = 'File is too large (max 10 MB)';
-            textEl_dropZoneError.classList.add('text-color-red');
+        if (resultContainer.classList.contains('hide')) {
+            dropZoneErrorMessage.textContent = 'File is too large (max 10 MB)';
+            dropZoneErrorMessage.classList.add('text-color-red');
         } else {
-            textEl_inputError.textContent = 'File is too large (max 10 MB)';
-            uiEl_urlError.classList.remove('hide');
+            fileInputErrorMessage.textContent = 'File is too large (max 10 MB)';
+            errorMessage.classList.remove('hide');
         }
     };
 
@@ -130,8 +111,8 @@ export const initAudio = () => {
 
     // Listeners
 
-    inputEl_fileInput?.addEventListener('change', () => {
-        const fileSize = inputEl_fileInput?.files[0].size;
+    fileInput?.addEventListener('change', () => {
+        const fileSize = fileInput?.files[0].size;
         const maxSize = 10 * 1024 * 1024; // 10 MB in bytes
         if (fileSize > maxSize) {
             fileSizeAllow = false;
@@ -144,15 +125,15 @@ export const initAudio = () => {
 
     const error_dropZone = () => {
         (document.querySelector('#processing-screen') as Element).classList.add('hide');
-        textEl_dropZoneError.classList.add('error');
-        uiEl_dropZone.classList.add('red-border');
-        textEl_dropZoneError.textContent = 'Something went wrong. Try again.';
+        dropZoneErrorMessage.classList.add('error');
+        dropZone.classList.add('red-border');
+        dropZoneErrorMessage.textContent = 'Something went wrong. Try again.';
     };
 
     const initial_dropZone = () => {
-        textEl_dropZoneError.classList.remove('error');
-        uiEl_dropZone.classList.remove('red-border');
-        textEl_dropZoneError.textContent = 'We support jpeg, png, webp, gif, tiff, bmp. 10 Mb of maximum size.';
+        dropZoneErrorMessage.classList.remove('error');
+        dropZone.classList.remove('red-border');
+        dropZoneErrorMessage.textContent = 'We support jpeg, png, webp, gif, tiff, bmp. 10 Mb of maximum size.';
     };
 
     const screen_homeShow = () => {
@@ -165,16 +146,12 @@ export const initAudio = () => {
         (document.querySelector('#hero-home_drop-zone-divider') as Element).classList.remove('hide');
         (document.querySelector('#result-screen_col') as Element).classList.add('hide');
         (document.querySelector('#result-screen_image-wrapper') as Element).classList.add('hide');
-        imageEl_currentImage.classList.add('hide');
-        imageEl_currentImageEmpty.classList.remove('hide');
-        imageEl_nsfwImage.classList.remove('hide');
     };
 
     const loadingStart = () => {
         uiReported_initialState();
-        imageEl_currentImage.src = '';
         someThingWentWrong_ok();
-        textEl_inputError.textContent = 'Something went wrong. Try again.';
+        fileInputErrorMessage.textContent = 'Something went wrong. Try again.';
         (document.querySelector('#choose-file-row') as Element).classList.remove('hide');
         (document.querySelector('#legal-tip') as Element).classList.add('hide');
         (document.querySelector('.processing-screen_triggers_5') as any).click();
@@ -189,17 +166,14 @@ export const initAudio = () => {
     };
 
     function loadingFinish() {
-        imageEl_nsfwImage.classList.add('hide');
-        imageEl_currentImage.classList.remove('hide');
-        imageEl_currentImageEmpty.classList.add('hide');
         // Show buttons for share the report.
-        buttonEl_sharedButtons.classList.remove('hide');
+        shareButtonsContainer.classList.remove('hide');
 
         (document.querySelector('.processing-screen_triggers_3') as any)?.click();
         (document.querySelector('#processing-screen') as Element).classList.add('hide');
         (document.querySelector('.processing-screen_triggers_5') as any)?.click();
         (document.querySelector('#scroll-to-top-trigger') as any)?.click();
-        inputEl_fileInput.value = '';
+        fileInput.value = '';
         youtubeLinkInput.value = '';
     }
 
@@ -239,20 +213,19 @@ export const initAudio = () => {
             signInModalElement.style.zIndex = 100;
             screen_homeShow();
         } else {
-            uiEl_urlError.classList.add('hide');
+            errorMessage.classList.add('hide');
             loadingStart();
 
             await WrapperAIGeneratedService.getYoutubeVerict(link)
                 .then((response) => {
                     // RequestCounter.increment();
                     changeShareUrl(response.id);
-                    // // imageEl_currentImage.src = link;
                     findHighestConfidence(response.report.verdict === true ? 'ai' : 'human');
                     loadingFinish();
                     fillYoutubePlayerCard(link);
                 })
                 .catch((error) => {
-                    if (uiEl_resultCol.classList.contains('hide')) {
+                    if (resultContainer.classList.contains('hide')) {
                         someThingWentWrong_error();
                     } else {
                         someThingWentWrong_error();
@@ -296,9 +269,9 @@ export const initAudio = () => {
         }
     });
 
-    inputEl_fileInput?.addEventListener('change', async (event: any) => {
+    fileInput?.addEventListener('change', async (event: any) => {
         if (fileSizeAllow == true) {
-            const file = inputEl_fileInput.files[0];
+            const file = fileInput.files[0];
             await uploadBinaryFile(file);
         } else {
             fileSizeMessage_error();
@@ -316,9 +289,6 @@ export const initAudio = () => {
                 // loadingFinish()
                 return;
             }
-
-            imageEl_currentImage.classList.remove('hide');
-            imageEl_currentImageEmpty.classList.add('hide');
 
             await WrapperAIGeneratedService.getAudioVerictByFile(file)
                 .then((response) => {
@@ -339,8 +309,6 @@ export const initAudio = () => {
 
     const tappedSampleAudio = async (url: string, name: string) => {
         loadingStart();
-        imageEl_currentImage.classList.remove('hide');
-        imageEl_currentImageEmpty.classList.add('hide');
 
         await WrapperAIGeneratedService.getAudioVerictMock(true).then((response) => {
             changeShareUrl(response.id);
@@ -351,17 +319,17 @@ export const initAudio = () => {
         });
     };
 
-    buttonEl_processClose?.addEventListener('click', function () {
+    cancelProcessingButton?.addEventListener('click', function () {
         initial_dropZone();
         screen_homeShow();
     });
 
     (document.querySelector('#ai-or-not_dropzone') as Element)?.addEventListener('click', function () {
-        inputEl_fileInput.click();
+        fileInput.click();
     });
 
     (document.querySelector('#choose-file-row') as Element)?.addEventListener('click', function () {
-        inputEl_fileInput.click();
+        fileInput.click();
     });
 
     checkYoutubeLinkButton?.addEventListener('click', () => {
@@ -392,24 +360,45 @@ export const initAudio = () => {
         }
     });
 
-    reportButton_true?.addEventListener('click', () => {
+    const uiReported_false = () => {
+        let buttonText = document.querySelector('#button-report_false-text') as Element;
+        buttonText.classList.remove('hide');
+        buttonText.textContent = buttonText.getAttribute('report-button-text-default_reported');
+        reportButtonDislike.classList.add('is-reported');
+        reportButtonLike.classList.add('hide');
+        reportScreen.style.display = 'none';
+    };
+
+    const uiReported_true = () => {
+        let buttonText = document.querySelector('#button-report_true-text') as Element;
+        buttonText.classList.remove('hide');
+        buttonText.textContent = buttonText.getAttribute('report-button-text-default_reported');
+        reportButtonLike.classList.add('is-reported');
+        reportButtonDislike.classList.add('hide');
+    };
+
+    reportButtonLike?.addEventListener('click', () => {
         uiReported_true();
         WrapperAIGeneratedService.sendFeedback(currentResultId, true, '', true);
     });
 
-    reportButton_false?.addEventListener('click', () => {});
-
-    reportButton_close?.addEventListener('click', () => {});
-
-    reportButton_submit?.addEventListener('click', () => {
-        WrapperAIGeneratedService.sendFeedback(currentResultId, false, reportInput.value, true);
-        uiReported_false();
+    reportButtonDislike?.addEventListener('click', () => {
+        reportScreen.style.display = 'flex';
     });
 
-    document?.addEventListener('keydown', function (event) {
+    reportScreenCloseButton?.addEventListener('click', () => {
+        reportScreen.style.display = 'none';
+    });
+
+    reportButton_submit?.addEventListener('click', () => {
+        uiReported_false();
+        WrapperAIGeneratedService.sendFeedback(currentResultId, false, reportInput.value, true);
+    });
+
+    document?.addEventListener('keydown', (event) => {
         if (event.code === 'Escape') {
             if (reportScreen.style.display !== 'none') {
-                reportButton_close.click();
+                reportScreenCloseButton.click();
             }
         }
     });
